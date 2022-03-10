0e2f0f80638ee8f606ee127a086c7ff5ece74afe
Kacper Łacniak <kkacper15@gmail.com>
2022-03-10 22:01:25 +0100

    main

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
 app.listen(4000, () =>
-  console.log('Visit http://127.0.0.1:4000')
+  console.log('Visit https://taidio1.github.io/Earth/')
 );
